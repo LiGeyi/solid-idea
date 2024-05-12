7669d4e53a248f7801afed9f92cb8c0c6e0134b8
LiGeyi <284712719@qq.com>
2024-05-12 17:24:04 +0800

    Add files via upload

---
+++ static/js/templates-slider.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	console.log("aaaa");
+	$(".template-flow-container").each(function(templateFlowContainer_index){
+		console.log("aaaa");
+		var templateFlowContainer = $(this);
+		var tagClassName = "flow_index_" + templateFlowContainer_index;
+		templateFlowContainer.addClass(tagClassName);
+		var isBackDirection = templateFlowContainer.hasClass("back_direction");
+
+		var groupWidth = 1934 * templateFlowContainer.find(".template-flow .template-set").length;
+		var containerWidth = templateFlowContainer.width();
+		var groupCount = 1 + Math.floor(containerWidth / groupWidth) + (containerWidth % groupWidth > 0 ? 1 : 0);
+		console.log(groupWidth + ", " + containerWidth + ", " + groupCount);
+		for(var i = 0; i < groupCount - 1; i++){
+			templateFlowContainer.find(".template-flow .template-set").not(".cloned").each(function(templateSet_index){
+				var left = templateSet_index * 1934;
+				$(this).css("left", (isBackDirection ? "-" : "" ) + left + "px");
+				$(this).clone().addClass("cloned").css("left",  (isBackDirection ? "-" : "" ) + (left + groupWidth * (i + 1)) + "px").appendTo(templateFlowContainer.find(".template-flow"));
+			});
+		}
+
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = "@keyframes ResMoveX_" + tagClassName + " {"
+					       + "	0% {"
+						   + "		transform: translate3d(0px, 0, 0);"
+						   + "  }"
+						   + "  100% {"
+						   + "  	transform: translate3d(" + (isBackDirection ? "" : "-") + groupWidth + "px, 0, 0);"
+						   + "  }"
+						   + "}"
+						   + ".template-flow-container." + tagClassName + " .template-flow{"
+						   + "  width:100%;"
+						   + "  animation: 45s linear 0s infinite normal forwards running ResMoveX_" + tagClassName + ";"
+						   + "  -webkit-perspective: 1000;"
+						   + "  -webkit-transform-style: preserve-3d;"
+						   + "}"
+						   + ".template-flow-container." + tagClassName + ", .template-flow-container." + tagClassName + " *{"
+						   + "  -webkit-backface-visibility: hidden;"
+						   + "}";
+		document.getElementsByTagName('head')[0].appendChild(style);
+	});
+});
